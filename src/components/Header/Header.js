a1a98d1c58171a92039e84a423d502d1db894261
lazycoder52 <aditya.arakali@gmail.com>
2026-05-13 13:00:25 +0530

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import "./Header.css";
+
+function Header({ onCartClick, onAddProductClick }) {
+  return (
+    <header className="header">
+      <h1>My React Store</h1>
+      <div>
+        <button
+          className="yellow-button"
+          onClick={onAddProductClick}
+          style={{ marginRight: "20px" }}
+        >
+          Add Product
+        </button>
+        <button className="yellow-button" onClick={onCartClick}>
+          Cart
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
